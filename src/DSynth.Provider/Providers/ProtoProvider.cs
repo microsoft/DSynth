0e96d64b186e37abe3a7bf9b8a1ca94b593eca5b
Steven Schneider <stschn@microsoft.com>
2023-10-29 08:32:57 -0700

    Using protobuf any

--- src/DSynth.Provider/Providers/ProtoProvider.cs
+++ src/DSynth.Provider/Providers/ProtoProvider.cs
@@ -10,6 +10,7 @@ using System.Threading;
 using DSynth.Common.Models;
 using System;
 using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
 
 namespace DSynth.Provider.Providers
 {
@@ -30,11 +31,12 @@ namespace DSynth.Provider.Providers
                 
                 // Dynamically create the appropriate class/message that
                 // gets converted into a byte array in protobuf encoding.
-                var type = Type.GetType(nextPayload.ProtoOptions.FullClassName);
+                var type = System.Type.GetType(nextPayload.ProtoOptions.FullClassName);
                 var message = (IMessage)Activator.CreateInstance(type);
                 string data = nextPayload.DataAsString;
                 var result = JsonParser.Default.Parse(data, message?.Descriptor);
-                var bytes = result.ToByteArray();
+                Any any = Any.Pack(result);
+                var bytes = any.ToByteArray();
 
                 return PayloadPackage.CreateNew(bytes, payloadCount, data);
             }
